2564c9d0e4509877865a11e47e878ecf3627a050
IndecentTulip <jowhite2727@gmail.com>
2025-01-10 14:30:52 -0800

    test changes

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+// App.js
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+const App = () => {
+  return (
+  <div>test</div>
+  );
+};
+
+export default App;
+
